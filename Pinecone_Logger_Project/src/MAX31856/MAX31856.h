bc085db69c0d5d9e3e5f83b5412b2d33ac82d4cf
Sawwave <timanderson.um@gmail.com>
2016-11-04 15:25:16 -0600

    added more documentation to max31856

--- Pinecone_Logger_Project/src/MAX31856/MAX31856.h
+++ Pinecone_Logger_Project/src/MAX31856/MAX31856.h
@@ -12,6 +12,9 @@
 #include <stdint.h>
 #include "spi/spi.h"
 
+
+//macros for each supported thermocouple type.
+//the value given is what will be set into the MAX31856 register.
 #define MAX31856_THERMOCOUPLE_TYPE_B	0x00
 #define MAX31856_THERMOCOUPLE_TYPE_E	0x01
 #define MAX31856_THERMOCOUPLE_TYPE_J	0x02
@@ -23,14 +26,25 @@
 
 enum Max31856_Status {MAX31856_OKAY, MAX31856_CONNECTION_ERROR, MAX31856_TEMP_TOO_HIGH, MAX31856_TEMP_TOO_LOW, MAX31856_TC_NOT_CONNECTED, MAX31856_FAULT_VOLTAGE, MAX31856_SPI_ERROR};
 
+/*Max31856ConfigureSPI
+	configures the given spi module and slave instance to be used with the MAX31856.*/
 void Max31856ConfigureSPI(struct spi_module *spiMasterModule, struct spi_slave_inst *spiSlaveInstance);
 
+/*Max31856CheckWrittenRegister
+	protyping helper function to write to a register, and check success by reading the register.*/
 enum Max31856_Status Max31856CheckWrittenRegister(struct spi_module *spiMasterModule, struct spi_slave_inst *slaveInst);
 
+/*Max31856ConfigureRegisters
+	communicates on the SPI to set the registers on the MAX31856 to the correct configuration*/
 enum Max31856_Status Max31856ConfigureRegisters(struct spi_module *spiMasterModule, struct spi_slave_inst *slaveInst, uint32_t thermocoupleType);
 
+/*Max31856RequestReading
+	Sends a message over SPI to request a reading from the Max31856. A delay of up to 800ms will be required before the reading is prepared. 
+	It is recommended to just enter standby mode for 1s while the sensor takes the measurement.*/
 enum Max31856_Status Max31856RequestReading(struct spi_module *spiMasterModule, struct spi_slave_inst *slaveInst);
 
+/*Max31856GetTemp
+	retrieves the temperature value from the Max31856,, loading it into the outTemp pointer*/
 enum Max31856_Status Max31856GetTemp(struct spi_module *spiMasterModule, struct spi_slave_inst *slaveInst, double *outTemp);
 
 #endif /* MAX31856_H_ */
